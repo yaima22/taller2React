e0a8dca2294d59837619dc9f5b28d5e82382c6d4
ander0898 <128049886+ander0898@users.noreply.github.com>
2023-05-18 21:27:51 -0500

    v1.2

--- talle2React/src/ui/components/Navbar.jsx
+++ talle2React/src/ui/components/Navbar.jsx
@@ -1,7 +1,71 @@
-import React from 'react'
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 export const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+
+    const navigate = useNavigate()
+
+    const onLogout = () =>{
+        // console.log('logout')
+        navigate('/login',{
+            replace: true
+        })
+    }
+
+    return (
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
+            
+            <Link 
+                className="navbar-brand" 
+                to="/"
+            >
+                Equipos
+            </Link>
+
+            <div className="navbar-collapse">
+                <div className="navbar-nav">
+
+                    <NavLink 
+                        className={({isActive}) =>`nav-item nav-link ${ isActive ? 'active' : '' }`} 
+                        to="/marvel"
+                    >
+                        Marvel
+                    </NavLink>
+
+                    <NavLink 
+                        className={({isActive}) =>`nav-item nav-link ${ isActive ? 'active' : '' }`}
+                        to="/dc"
+                    >
+                        DC
+                    </NavLink>
+                    <NavLink 
+                        className={({isActive}) =>`nav-item nav-link ${ isActive ? 'active' : '' }`}
+                        to="/search"
+                    >
+                        Buscar
+                    </NavLink>
+                    <NavLink 
+                        className={({isActive}) =>`nav-item nav-link ${ isActive ? 'active' : '' }`}
+                        to="/hero"
+                    >
+                        Heroe
+                    </NavLink>
+                </div>
+            </div>
+
+            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
+                <ul className="navbar-nav ml-auto">
+                    <span className='nav-item nav-link text-primary'>
+                        yaima
+                    </span>
+
+                    <button
+                        className='van-item nav-link btn'
+                        onClick={ onLogout }
+                    >
+                        Logout
+                    </button>
+                </ul>
+            </div>
+        </nav>
+    )
 }
